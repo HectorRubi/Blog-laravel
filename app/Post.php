fa841b72fe73beea4039a1e0f7cb97f6e8ff6ba9
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-11-28 14:11:35 -0600

    Add feature to show embebed code in posts list and post content

--- app/Post.php
+++ app/Post.php
@@ -32,4 +32,16 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function getGetExcerptAttribute()
+    {
+        return substr($this->body, 0, 140);
+    }
+
+    public function getGetImageAttribute()
+    {
+        if ($this->image) {
+            return url("storage/$this->image");
+        }
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,13 @@
 <div class="container">
     <div class="card mb-4">
         <div class="card-body">
+            @if($post->image)
+                <img src="{{ $post->get_image }}" alt="Imagen" class="card-img-top">
+            @elseif($post->iframe)
+                <div class="embed-responsive embed-responsive-16by9">
+                    {!! $post->iframe !!}
+                </div>
+            @endif
             <h3 class="card-title">{{ $post->title }}</h3>
             <p class="card-text">
                 {{ $post->body }}
